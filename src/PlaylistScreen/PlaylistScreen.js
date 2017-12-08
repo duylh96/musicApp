8031c0b805f59437a19383addd4dca0ffa72f544
Hoang Duy <duylh.1996@gmail.com>
2017-12-08 08:39:44 +0700

    fix playlist screen issue not display compatitble

--- src/PlaylistScreen/PlaylistScreen.js
+++ src/PlaylistScreen/PlaylistScreen.js
@@ -1,63 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Image
 } from 'react-native';
 
-class ListSong extends Component<{}> {
+export default class PlaylistScreen extends Component < {} > {
 
-
-render() {
+    render() {
         return (
-        <TouchableOpacity>
-            <View style={css.renBox}>
-                <Image
-                style={css.icon}
-                    source={{uri: 'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg'}}/>
-                <View style={css.renItem}>
-                    <Text style={css.tieude1}> Devide </Text>
+            <TouchableOpacity>
+                <View style={css.renBox}>
+                    <Image
+                        style={css.icon}
+                        source={{
+                        uri: 'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg'
+                    }}/>
+                    <View style={css.renItem}>
+                        <Text style={css.tieude1}>
+                            Devide
+                        </Text>
 
-                    <Text style={css.tieude2}> Ed Sheeran</Text>
+                        <Text style={css.tieude2}>
+                            Ed Sheeran</Text>
+                    </View>
                 </View>
-            </View>
-          </TouchableOpacity>
+            </TouchableOpacity>
         );
     }
 }
 
 var css = StyleSheet.create({
-    icon:{
-        width:60,
-        height:60,
+    icon: {
+        width: 60,
+        height: 60
     },
-    tieude1:{
-        backgroundColor:'white',
-        color:'black',
-        flex:2,
+    tieude1: {
+        backgroundColor: 'white',
+        color: 'black',
+        flex: 2
     },
-    tieude2:{
-        backgroundColor:'white',
-        color:'grey',
-        flex:1,
+    tieude2: {
+        backgroundColor: 'white',
+        color: 'grey',
+        flex: 1
     },
-    renBox:{
-        backgroundColor:'white',
-        borderBottomWidth:1,
-        flexDirection:'row',
-        height:60,
-
+    renBox: {
+        backgroundColor: 'white',
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        height: 60
     },
-    renItem:{
-        borderBottomWidth:1,
-        flexDirection:'column',
+    renItem: {
+        borderBottomWidth: 1,
+        flexDirection: 'column'
     },
-    border:{
-        borderBottomWidth:1,
-        flex:1,
+    border: {
+        borderBottomWidth: 1,
+        flex: 1
     }
 });
