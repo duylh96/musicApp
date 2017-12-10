4edb3afcc961b8c394750703eb690317b0d403eb
sheldon190596 <sheldon19051996@gmail.com>
2017-12-10 21:08:11 +0700

    Update PlaylistScreen
    Add Listview

--- src/PlaylistScreen/PlaylistScreen.js
+++ src/PlaylistScreen/PlaylistScreen.js
@@ -6,43 +6,72 @@ import {
     Text,
     View,
     TouchableOpacity,
-    Image
+    Image,
+    ListView
 } from 'react-native';
 
 export default class PlaylistScreen extends Component < {} > {
+    constructor() {
+        super();
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            //dataSource: ds.cloneWithRows(['a', 'b', 'c', 'a longer example', 'd', 'e']),
+            dataSource: ds.cloneWithRows(this.genRows({}))
+        };
+      }
+      genRows(pressData: {[key: number]: boolean}): Array<string> {
+
+          var data = [];
+
+          for (var ii = 0; ii < 50; ii++) {
+
+            data.push('Row ' + ii);
+
+          }
+
+          return data;
+
+        }
 
     render() {
         return (
-            <TouchableOpacity>
-                <View style={css.renBox}>
-                    <Image
-                        style={css.icon}
-                        source={{
-                        uri: 'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg'
-                    }}/>
-                    <View style={css.renItem}>
-                        <Text style={css.tieude1}>
-                            Devide
-                        </Text>
+//            <TouchableOpacity>
+//                <View style={css.renBox}>
+//                    <Image
+//                        style={css.icon}
+//                        source={{
+//                        uri: 'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg'
+//                    }}/>
+//                    <View style={css.renItem}>
+//                        <Text style={css.tieude1}>
+//                            Deviderr
+//                        </Text>
+//
+//                        <Text style={css.tieude2}>
+//                            Ed Sheeran</Text>
+//                    </View>
+//                </View></TouchableOpacity>
+                 <ListView
+                        dataSource={this.state.dataSource}
+                        renderRow={(rowData) => <Text>{rowData}</Text>}
+                      />
 
-                        <Text style={css.tieude2}>
-                            Ed Sheeran</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
         );
     }
 }
 
+
+
+
 var css = StyleSheet.create({
     icon: {
-        width: 60,
-        height: 60
+        width: 100,
+        height: 100
     },
     tieude1: {
         backgroundColor: 'white',
         color: 'black',
-        flex: 2
+        flex: 1
     },
     tieude2: {
         backgroundColor: 'white',
@@ -51,9 +80,9 @@ var css = StyleSheet.create({
     },
     renBox: {
         backgroundColor: 'white',
-        borderBottomWidth: 1,
-        flexDirection: 'row',
-        height: 60
+        flexDirection: 'column',
+        height: 100,
+        width: 200
     },
     renItem: {
         borderBottomWidth: 1,
