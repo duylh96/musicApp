54047d635910bc8293d5476eb5aa71ba7a3257d0
sheldon190596 <sheldon19051996@gmail.com>
2018-01-01 10:14:57 +0700

    Update BXHScreen.js
    Add chart image instead using boring number

--- src/BXHScreen/BXHScreen.js
+++ src/BXHScreen/BXHScreen.js
@@ -11,15 +11,15 @@ export default class BXHScreen extends Component {
     render() {
 		const song_source = [			
 			//music images
-        {url:'https://avatar-nct.nixcdn.com/song/2017/10/27/9/d/8/d/1509093543890.jpg',Name:'All Falls Down', Artist: 'Alan Walker, Noah Cyrus', id: 1,},
-		{Name:"How Long", Artist: 'Charlie Puth', id: 2},
-		{Name:"The Spectre", Artist: 'Alan Walker', id: 3},
+        {url:'https://avatar-nct.nixcdn.com/song/2017/10/27/9/d/8/d/1509093543890.jpg',Name:'All Falls Down', Artist: 'Alan Walker, Noah Cyrus', id: 1, id_img:'https://images.vexels.com/media/users/3/127646/isolated/preview/bb4029ca0e2f6ffb36a5c80dba5e54e0-first-place-round-badge-by-vexels.png'},
+		{Name:"How Long", Artist: 'Charlie Puth', id: 2,id_img:'https://cdn.iconscout.com/public/images/icon/premium/png-512/badge-second-place-reward-award-33f61a015d97305e-512x512.png'},
+		{Name:"The Spectre", Artist: 'Alan Walker', id: 3, id_img: 'http://images.easyfreeclipart.com/1223/back-gt-gallery-for-6th-place-ribbon-clip-art-1223092.png'},
 		];
 		const playlist_source = [			
 			//music images
-        {url:'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg',Name:'Divide', Artist: 'Ed Sheeran', id: 1},
-		{url:'https://avatar-nct.nixcdn.com/playlist/2017/11/08/c/c/b/f/1510125100417.jpg',Name:"Perfect", Artist: 'Taylor Swift', id: 2},
-		{url:'https://avatar-nct.nixcdn.com/playlist/2017/12/15/9/1/c/5/1513321050320.jpg',Name:"Revival", Artist: 'Eminem', id: 3},
+        {url:'https://pbs.twimg.com/profile_images/819500159365160960/AOneM0y3_400x400.jpg',Name:'Divide', Artist: 'Ed Sheeran', id: 1, id_img:'https://images.vexels.com/media/users/3/127646/isolated/preview/bb4029ca0e2f6ffb36a5c80dba5e54e0-first-place-round-badge-by-vexels.png'},
+		{url:'https://avatar-nct.nixcdn.com/playlist/2017/11/08/c/c/b/f/1510125100417.jpg',Name:"Perfect", Artist: 'Taylor Swift', id: 2, id_img:'https://cdn.iconscout.com/public/images/icon/premium/png-512/badge-second-place-reward-award-33f61a015d97305e-512x512.png'},
+		{url:'https://avatar-nct.nixcdn.com/playlist/2017/12/15/9/1/c/5/1513321050320.jpg',Name:"Revival", Artist: 'Eminem', id: 3, id_img: 'http://images.easyfreeclipart.com/1223/back-gt-gallery-for-6th-place-ribbon-clip-art-1223092.png'},
 		];
        
 
@@ -33,7 +33,6 @@ export default class BXHScreen extends Component {
 								<Icon name="md-arrow-forward"/>
 							</Button>
 						  </View>
-						   
 						  <Button 
 								style={styles.icon}
 						  		block 
@@ -46,14 +45,14 @@ export default class BXHScreen extends Component {
 							renderRow={item => <ListItem >
 							<Button 
 								//block 
-								transparent androidRippleColor height={30}>
+								transparent androidRippleColor >
 								<View style={styles.item_container}>
 									<View style={styles.item_container_left}>
-										<Text
+										<Image
 											style={{
 											alignSelf: 'center',
-											color: Math.random(),
-										}}>{item.id}</Text>
+											height: 45, width: 45,
+										}} source= {{uri: item.id_img}}/>
 										<Image source={{uri: item.url}} style={styles.icon}/>
 										<View style={styles.title}>
 											<Text style={styles.text_title} >{item.Name}</Text>
@@ -64,7 +63,6 @@ export default class BXHScreen extends Component {
 								</View>
 							</Button>
 						</ListItem>}></List>
-					
 					</Content>
 					<Content style={styles.content_styles}>
 							<View style={styles.bxh_header}>
@@ -83,14 +81,14 @@ export default class BXHScreen extends Component {
 							dataArray={playlist_source}
 							renderRow={item => <ListItem >
 							<Button 
-								transparent androidRippleColor height={30}>
+								transparent androidRippleColor >
 								<View style={styles.item_container}>
 									<View style={styles.item_container_left}>
-										<Text
+										<Image
 											style={{
 											alignSelf: 'center',
-											color: Math.random(),
-										}}>{item.id}</Text>
+											height: 45, width: 45,
+										}} source= {{uri: item.id_img}}/>
 										<Image source={{uri: item.url}} style={styles.icon}/>
 										<View style={styles.title}>
 											<Text style={styles.text_title}>{item.Name}</Text>
@@ -112,12 +110,12 @@ export default class BXHScreen extends Component {
 const styles = StyleSheet.create({
 	 render:{
 		flexDirection: 'row',
-		
      },
 	 bxh_text_header: {
 		marginLeft: 10,
     	fontSize: 18,
-    	fontWeight: '200'
+    	fontWeight: '200',
+		height: 30,
   	 },
 	 bxh_header: {
 		flexDirection: 'row',
@@ -128,8 +126,7 @@ const styles = StyleSheet.create({
 		borderStyle: 'solid',
 		borderWidth: 8,
 		marginTop: 18,
-		marginBottom: 8,
-		height: 60,
+		height: 80,
 		zIndex:1
 	 },
 	 list:{
@@ -145,22 +142,22 @@ const styles = StyleSheet.create({
      },
 	 icon:{
 		position: 'absolute',
-		height: 150,
-		width: 110,
+		height: 180,
+		width: 130,
 		flex: 1,
-		marginTop: 95,
+		marginTop: 125,
 		zIndex:2,
-		
 	 },
 
     item_container: {
+		alignItems: 'baseline',
         flexDirection: 'column',
         width: '100%',
         justifyContent: 'space-between',
-        marginLeft: 100
+        marginLeft: 120,
     },
     item_container_left: {
-        flexDirection: 'row'
+        flexDirection: 'row',
     },
     title: {
         marginLeft: 15,
@@ -170,7 +167,7 @@ const styles = StyleSheet.create({
     text_title: {
         fontWeight: '200',
         fontSize: 13,
-        marginBottom: 5,
+        //marginBottom: 5,
     },
     text_singer: {
         fontWeight: '100',
