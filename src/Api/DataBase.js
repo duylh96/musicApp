3945d0df5115bb1d67934f38984040193ab0a5dc
hoangduy1996 <duylh.1996@gmail.com>
2018-01-02 17:02:19 +0700

    Fix issue with play on real device

--- src/Api/DataBase.js
+++ src/Api/DataBase.js
@@ -1,4 +1,6 @@
-import Song from './Song';
+import Sound from 'react-native-sound';
+Sound.setCategory('Playback');
+
 export var listSong = [
     {
         id: 0,
@@ -6,7 +8,7 @@ export var listSong = [
         singer: 'Đức Phúc',
         color: 'purple',
         url: require('../Images/Albums/0.jpg'),
-        content: new Song(require('../Musics/nam_ay.mp3')),
+        content: new Sound('nam_ay.mp3', Sound.MAIN_BUNDLE, (error) => {}),
         image: require('../Images/BackDrop/nam_ay.jpg'),
         backgroundColor: '#2b2117'
 
@@ -16,7 +18,7 @@ export var listSong = [
         singer: 'Châu Khải Phong',
         color: 'green',
         url: require('../Images/Albums/1.jpg'),
-        content: new Song(require('../Musics/nguoi_yeu_co_ay.mp3')),
+        content: new Sound('nguoi_yeu_co_ay.mp3', Sound.MAIN_BUNDLE, (error) => {}),
         image: require('../Images/BackDrop/nguoi_yeu_co_ay.jpg'),
         backgroundColor: '#7eb5c9'
 
@@ -26,7 +28,7 @@ export var listSong = [
         singer: 'Nguyễn Hải Yến',
         color: 'orange',
         url: require('../Images/Albums/2.jpg'),
-        content: new Song(require('../Musics/dem_lao_xao.mp3')),
+        content: new Sound('dem_lao_xao.mp3', Sound.MAIN_BUNDLE, (error) => {}),
         image: require('../Images/BackDrop/tophit_hai_yen.jpg'),
         backgroundColor: '#5a4a4a'
 
@@ -36,7 +38,7 @@ export var listSong = [
         singer: 'Hương Tràm',
         color: 'gray',
         url: require('../Images/Albums/3.jpg'),
-        content: new Song(require('../Musics/canh_hoa_tan.mp3')),
+        content: new Sound('canh_hoa_tan.mp3', Sound.MAIN_BUNDLE, (error) => {}),
         image: require('../Images/BackDrop/canh_hoa_tan.jpg'),
         backgroundColor: '#521419'
 
@@ -46,7 +48,7 @@ export var listSong = [
         singer: 'Bùi Anh Tú',
         color: 'gray',
         url: require('../Images/Albums/4.jpg'),
-        content: new Song(require('../Musics/let_me_know.mp3')),
+        content: new Sound('let_me_know.mp3', Sound.MAIN_BUNDLE, (error) => {}),
         image: require('../Images/BackDrop/let_me_know.jpg'),
         backgroundColor: '#626262'
 
